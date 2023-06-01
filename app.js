56d69ccad73999d119f1780ec66d014bce929bb6
SGhoshh <116429160+SGhoshh@users.noreply.github.com>
2023-06-01 19:10:36 +0530

    feat : Server integration
    Learned to hook up the app with a server

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output");
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranlationURL(text){
+    return serverUrl + "?" + "text="+ text;
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+}
 
 function btnclick() {
-    txtOutput.innerText = "Translated text will appear here "+txtInput.value;
+    var Input= txtInput.value // text in the input text area
+
+    fetch(getTranlationURL(Input))
+    .then(response => response.json)
+    .then(json => console.log(json.contents.text))
+    .catch(errorHandler);
 }
 btnTranslate.addEventListener("click", btnclick);
+
+
